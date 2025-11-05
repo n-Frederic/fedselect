d2cdf71edeb2cbc4fce217c858621aa77fdeabeb
mark2330 <ma18201500146@gmail.com>
2025-11-05 19:45:38 +0800

    训练修改

--- utils/options.py
+++ utils/options.py
@@ -28,7 +28,7 @@ def lth_args_parser():
     parser.add_argument("--prune_target", default=80, type=int, help="Pruning target")
     parser.add_argument(
         # "--com_rounds", type=int, default=4, help="rounds of fedavg training"
-        "--com_rounds", type=int, default=8, help="rounds of fedavg training"
+        "--com_rounds", type=int, default=20, help="rounds of fedavg training"
     )
     parser.add_argument(
         "--la_epochs",
@@ -37,14 +37,14 @@ def lth_args_parser():
         help="rounds of training for local alt optimization",
     )
     parser.add_argument("--iid", action="store_true",default=True, help="whether i.i.d or not")
-    parser.add_argument("--num_users", type=int, default=30, help="number of users: K")
+    parser.add_argument("--num_users", type=int, default=20, help="number of users: K")
     parser.add_argument(
         "--shard_per_user", type=int, default=2, help="classes per user"
     )
     parser.add_argument("--local_bs", type=int, default=32, help="local batch size: B")
     parser.add_argument(
         # "--frac", type=float, default=0.1, help="the fraction of clients: C"
-        "--frac", type=float, default=0.1, help="the fraction of clients: C"
+        "--frac", type=float, default=0.5, help="the fraction of clients: C"
 
     )
     parser.add_argument("--num_classes", type=int, default=10, help="number of classes")
